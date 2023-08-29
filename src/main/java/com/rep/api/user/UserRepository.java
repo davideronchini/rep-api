3136a8b727a2f03ec5981acb4d3a64921fde3f6d
Davide Ronchini <ronchinidavid3@gmail.com>
2023-08-29 17:56:51 +0200

    Changed user id type from Integer to Long

--- src/main/java/com/rep/api/user/UserRepository.java
+++ src/main/java/com/rep/api/user/UserRepository.java
@@ -1,11 +1,10 @@
 package com.rep.api.user;
 
-import com.rep.api.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 }
