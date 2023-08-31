464573f2e22d8deb4fa0bcd4541f72c95aea3cd2
Davide Ronchini <ronchinidavid3@gmail.com>
2023-08-31 19:09:33 +0200

    Cleaned up the code

--- src/main/java/com/rep/api/interaction/InteractionRepository.java
+++ src/main/java/com/rep/api/interaction/InteractionRepository.java
@@ -1,6 +1,5 @@
 package com.rep.api.interaction;
 
-import com.rep.api.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
